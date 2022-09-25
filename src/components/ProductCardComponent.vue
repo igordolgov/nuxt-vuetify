b7b7a6ef6b2766c0226fe42c6f081df6a9c431ce
Igor Dolgov <igordolgov1972@gmail.com>
2022-09-26 02:26:19 +0300

    modifying

--- src/components/ProductCardComponent.vue
+++ src/components/ProductCardComponent.vue
@@ -1,9 +1,9 @@
 ﻿<template lang="pug">
-v-col.pa-1(cols="12" sm="6" md="4" xl="3")
-  router-link(:to="{ name: 'product', params: { id: product.id } }").decor
+v-col.pa-1(cols="12", sm="6", md="4", xl="3")
+  router-link.decor(:to="{ name: 'product', params: { id: product.id } }")
     v-card.pa-2(elevation="4", height="100%")
       v-row
-        v-col(cols="6" sm="6" lg="8").pr-2
+        v-col.pr-2(cols="6", sm="6", lg="8")
           v-img.mb-1.grey.lighten-3(
             :src="product.image",
             :alt="product.title",
@@ -11,7 +11,7 @@ v-col.pa-1(cols="12" sm="6" md="4" xl="3")
             aspect-ratio="1",
             height="auto"
           )
-        v-col.pl-0.text-body-2.lh(cols="6" sm="6" lg="4")
+        v-col.pl-0.text-body-2.lh(cols="6", sm="6", lg="4")
           p.title-height.mb-0.font-weight-bold
             | {{ product.title }}
           p.pa-0.ma-0(v-if="product.quantity > 0")
@@ -47,13 +47,13 @@ export default {
 </script>
 
 <style>
-  .title-height {
-    min-height: 2.8em;
-  }
-  .decor {
-    text-decoration: none !important;
-  }
-  .lh {
-    line-height: 1.3 !important;
-  }
+.title-height {
+  min-height: 2.8em;
+}
+.decor {
+  text-decoration: none !important;
+}
+.lh {
+  line-height: 1.3 !important;
+}
 </style>
